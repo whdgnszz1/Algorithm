6654a86b35de58b2c689055ee326a04b50e5991e
whdgnszz1 <115535910+whdgnszz1@users.noreply.github.com>
2023-04-09 16:28:23 +0900

    [Bronze I] Title: 세로읽기, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.js
@@ -0,0 +1,14 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let lenArr = input.map(el => el.length);
+let maxLen = Math.max(...lenArr);
+let result = '';
+
+for (let i = 0; i < maxLen; i++) {
+	for (let j = 0; j < input.length; j++) {
+		if (input[j][i] === undefined) continue;
+		else result += input[j][i];
+	}
+}
+
+console.log(result);
