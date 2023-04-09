b89eea8ad502e98235a1a2334eeb51a715a3b999
whdgnszz1 <115535910+whdgnszz1@users.noreply.github.com>
2023-04-09 14:18:48 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("");
+let reverseArr = [...input].reverse()
+
+console.log(input.join("") === reverseArr.join("") ? 1 : 0)
