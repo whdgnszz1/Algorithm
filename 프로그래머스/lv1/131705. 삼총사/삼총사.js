80cd33a6d98faaefe06144c5c04fe5a6f28292f7
whdgnszz1 <115535910+whdgnszz1@users.noreply.github.com>
2023-05-01 19:48:43 +0900

    [level 1] Title: 삼총사, Time: 0.14 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -3,12 +3,9 @@ function solution(number) {
     for(let i = 0 ; i < number.length ; i++) {
         for(let j = i + 1 ; j < number.length; j++) {
             for(let k = j + 1; k < number.length; k++) {
-                if(number[i] + number[j] + number[k] === 0) {
-                    answer++
-                }
+                if(number[i] + number[j] + number[k] === 0) answer++
             }
         }
     }
-    
     return answer
 }
