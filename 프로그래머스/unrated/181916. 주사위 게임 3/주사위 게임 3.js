bc3a7fd8ac3aee77fd9dd281b2aa0b7d9bf379a8
whdgnszz1 <115535910+whdgnszz1@users.noreply.github.com>
2023-06-02 00:45:13 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
@@ -0,0 +1,19 @@
+function solution(a, b, c, d) {
+    var arr = [a,b,c,d];
+    let obj = new Map()
+    arr.map((item) => {
+        obj.has(item)? obj.set(item,obj.get(item)+1) : obj.set(item,1)
+    })
+    let sortedArr = [...obj].sort((a,b) => b[1]-a[1])
+    if(sortedArr.length === 1) {
+        return 1111 * sortedArr[0][0]
+    } else if (sortedArr.length === 2 && sortedArr[0][1]===3) {
+        return Math.pow(10 * sortedArr[0][0] + sortedArr[1][0], 2)
+    } else if (sortedArr.length === 2) {
+        return (sortedArr[0][0] + sortedArr[1][0]) * (Math.abs(sortedArr[0][0] - sortedArr[1][0]))
+    } else if (sortedArr.length === 3) {
+        return (sortedArr[1][0] * sortedArr[2][0])
+        } else {
+            return sortedArr.map(v => v[0]).sort((a,b) => a-b)[0]
+        }
+}
