ed96a2ca30a57dac214aa7723145d48f9c9a30d4
whdgnszz1 <115535910+whdgnszz1@users.noreply.github.com>
2023-06-22 15:11:37 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,10 @@
+function solution(t, p) {
+    var answer = 0;
+    
+    for (let i = 0 ; i < t.length - p.length + 1 ; i++) {
+        if(Number(t.substr(i , p.length)) <= Number(p)) {
+            answer++
+        }
+    }
+    return answer;
+}
