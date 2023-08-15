ba4b5ff7be5368c9094cd5c5f60c6f0f5a94378f
whdgnszz1 <115535910+whdgnszz1@users.noreply.github.com>
2023-08-15 23:02:29 +0900

    [unrated] Title: 배열 만들기 2, Time: 127.01 ms, Memory: 51.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,5 +1,6 @@
 function solution(l, r) {
     var answer = [];
+                // 1 ~ 200000 배열   ==> [ 5, 50, 55 , 500 , ... 1000000]
     let arr = Array.from({length:200000}, (_,index) => index+1).map(v => v.toString(2)* 5)
     let filteredArr = arr.filter(v => 
         v >= l && v <= r
