d223acb700608558ffaa714c2b5efb6be7b85d89
whdgnszz1 <115535910+whdgnszz1@users.noreply.github.com>
2023-08-19 16:24:37 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,12 +1,11 @@
 function solution(array, commands) {
-    var answer = [];
-    let arr = [...array]
-    for (i = 0 ; i < commands.length; i++) {
-        let a = commands[i][0]
-        let b = commands[i][1]
-        let c = commands[i][2]
-        let d = arr.slice(a-1,b).sort((a,b) => a - b)
-        answer.push(d[c-1])
-    }
-    return answer;
+  //return 할 배열 만들기
+  let answer = []
+  commands.forEach((a) => {
+    // command에 따라서 잘라넣을 배열
+    let arr = array.slice(a[0] - 1, a[1])
+    arr.sort((a,b) => a- b);
+    answer.push(arr[a[2] - 1])
+  })
+  return answer;
 }
