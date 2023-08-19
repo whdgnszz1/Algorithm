0e2149918d8923f4a62f92731f58b4bb39cc3bc0
whdgnszz1 <115535910+whdgnszz1@users.noreply.github.com>
2023-08-19 17:09:01 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4.07 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,14 +1,17 @@
 function solution(s) {
-    var answer = [];
-    let arr = s.split("").map((v,i) => [v,i])
-    s.split("").map((v,i) => {
-        if(i === s.split("").indexOf(v)) {
-            answer.push(-1)
-        } else {
-            let filteredArr = arr.filter(item => item[0] === v).map(v => v[1])
-            answer.push(i - filteredArr[filteredArr.indexOf(i) - 1])
-            
-        }
-    })
-    return answer;
+  let answer = []
+  let sArr = s.split("")
+  let pushArr = []
+  sArr.forEach((a) => {
+    let prevLastIndex = pushArr.lastIndexOf(a) 
+    if(prevLastIndex === -1) {
+      pushArr.push(a)
+      answer.push(-1)
+    } else {
+      pushArr.push(a)
+      let nextLastIndex = pushArr.lastIndexOf(a)
+      answer.push(nextLastIndex - prevLastIndex)
+    }
+  })
+  return answer;
 }
