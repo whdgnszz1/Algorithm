05f707799fbb9d21457636f9e7e30b1889679609
whdgnszz1 <115535910+whdgnszz1@users.noreply.github.com>
2023-08-19 15:24:13 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,30 +1,9 @@
-function solution(s) {
-    let obj = {
-        "zero": 0,
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9
-    }
-    if (Number(s) === Number) {
-        return Number(s)
-    } else {
-        s = s.replace(/zero/g, 0)
-        s = s.replace(/one/g, 1)
-        s = s.replace(/two/g, 2)
-        s = s.replace(/three/g, 3)
-        s = s.replace(/four/g, 4)
-        s = s.replace(/five/g, 5)
-        s = s.replace(/six/g, 6)
-        s = s.replace(/seven/g, 7)
-        s = s.replace(/eight/g, 8)
-        s = s.replace(/nine/g, 9)
-    
-        } 
-        return Number(s)
+const solution = (s) => {
+  const numArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+  numArr.forEach((a, i)=> {
+    s = s.replaceAll(a, i)
+  })
+
+  return +s
 }
