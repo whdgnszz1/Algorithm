a0c3ffb5cfc2b948b5d016c50334e2e9abc79818
whdgnszz1 <115535910+whdgnszz1@users.noreply.github.com>
2023-08-23 11:52:49 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,28 +1,31 @@
-function solution(n, lost, reserve) {
-    let arr = Array.from({length:n} ,(_, index) => index = 1);
+function solution(n, lost, reserve){
+  let count = 0
 
-    // 여벌 체육복이 있는 학생
-    for (let j = 0; j < reserve.length; j++) {
-        arr[reserve[j] - 1] += 1;
+  for(let i = 0 ; i < lost.length; i++) {
+    if(reserve.indexOf(lost[i]) !== -1){
+      let reserveIndex = reserve.indexOf(lost[i])
+      reserve.splice(reserveIndex, 1)
+      lost.splice(i, 1)
+      i -= 1
+    }
+  }
+
+  lost.sort((a,b) => a-b)
+
+  for(let i = 0; i < lost.length; i++) {
+    if(reserve.indexOf(lost[i] - 1) !== -1) {
+      let reserveIndex = reserve.indexOf(lost[i] - 1)
+      reserve.splice(reserveIndex, 1)
+      continue
     }
 
-    // 체육복이 도난당한 학생
-    for (let i = 0; i < lost.length; i++) {
-        arr[lost[i] - 1] -= 1;
+    if(reserve.indexOf(lost[i] + 1) !== -1) {
+      let reserveIndex = reserve.indexOf(lost[i] + 1)
+      reserve.splice(reserveIndex, 1)
+      continue
     }
 
-    // 체육복 빌려주기
-    for (let k = 0; k < arr.length; k++) {
-        if (arr[k] === 0) {
-            if (arr[k - 1] === 2) {
-                arr[k - 1] -= 1;
-                arr[k] += 1;
-            } else if (arr[k + 1] === 2) {
-                arr[k + 1] -= 1;
-                arr[k] += 1;
-            }
-        }
-    }
-
-    return arr.filter((v) => v > 0).length;
+    count++
+  }
+  return n - count
 }
